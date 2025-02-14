3e5428c5fdbb866402b18fe2a506e7da37447813
JlCln <j.coulon.pro@gmail.com>
2025-02-14 15:35:14 +0100

    first commit

---
+++ BlazorValentineApp/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.{razor,html,cshtml}"],
+  theme: {
+    extend: {
+      colors: {
+        valentine: {
+          light: "#ff758f",
+          dark: "#ff1744",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
